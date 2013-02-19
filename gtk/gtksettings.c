b57c5635e85755295f10eaaa951d9653c8560fa7
Matt Sealey <matt@genesi-usa.com>
2013-02-18 18:11:05 -0600

    don't default to Raleigh theme
    Signed-off-by: Matt Sealey <matt@genesi-usa.com>

--- gtk/gtksettings.c
+++ gtk/gtksettings.c
@@ -292,7 +292,7 @@ gtk_settings_class_init (GtkSettingsClass *class)
                                              g_param_spec_string ("gtk-theme-name",
 								   P_("Theme Name"),
 								   P_("Name of theme RC file to load"),
-								  "Raleigh",
+								  "Clearlooks",
 								  GTK_PARAM_READWRITE),
                                              NULL);
   g_assert (result == PROP_THEME_NAME);
